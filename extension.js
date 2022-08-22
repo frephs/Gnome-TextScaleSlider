fc0c06f46dcc3a3a4b8d791b934ad693d4b09eef
Francesco Genovese <ffrancescogenovese@gmail.com>
2022-08-22 19:51:11 +0200

    feat: Implementing slider (untested)

--- extension.js
+++ extension.js
@@ -1,4 +1,10 @@
+//imports
 const {Gio, GLib, GObject, St} = imports.gi;
+
+// screen and main functionality
+const Main = imports.ui.main;
+const Clutter = imports.gi.Clutter;
+
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 const Slider = imports.ui.slider;
@@ -9,63 +15,83 @@ const Me = imports.misc.extensionUtils.getCurrentExtension();
 const {panel} = imports.ui.main;
 
 
-function getSettings() {
+function _getGnomeSettings() {
   let GioSSS = Gio.SettingsSchemaSource;
-  let schemaSource = GioSSS.new_from_directory(
-    "/usr/share/glib-2.0/schemas",
-    GioSSS.get_default(),
-    false
-  );
-  let schemaObj = schemaSource.lookup(
-    'org.gnome.desktop.interface', true);
+  let schemaSource = GioSSS.new_from_directory("/usr/share/glib-2.0/schemas", GioSSS.get_default(), false);
+  let schemaObj = schemaSource.lookup('org.gnome.desktop.interface', true);
+  
   if (!schemaObj) {
     throw new Error('cannot find schemas');
   }
   return new Gio.Settings({ settings_schema : schemaObj });
 }
 
-// function init() {
 
-//   let settings = getSettings();
-  
-
-// }
-
-// function enable() {	
-//     let settings = getSettings();
-//     settings.set_double('text-scaling-factor', 1.2);
-//     log( "my double:" + settings.get_double('text-scaling-factor').toString() );
-  
-// }
-
-// function disable() {
-//     let settings = getSettings();
-//     settings.set_double('text-scaling-factor', 1.0);
-
-// }
-
-
-class Extension {
-	constructor(uuid) {
-		this._uuid = uuid;
-
-		ExtensionUtils.initTranslations();
-	}
-
-	enable() {
-		lg("start");
-		this._indicator = new Indicator();
-		Main.panel.addToStatusArea(this._uuid, this._indicator);
-	}
-
-	disable() {
-		lg("stop");
-		this._indicator.destroy();
-		this._indicator = null;
-		Main.wm.removeKeybinding("open-last-file-in-termianl");
-	}
+function _getInitialSliderValue() {
+	return gnomeSettings.get_double('text-scaling-factor').toString() 
 }
 
-function init(meta) {
-	return new Extension(meta.uuid);
+
+function _onSliderChanged() {
+	gnomeSettings.set_double('text-scaling-factor', slider.value);
+
+    // gnomeSettings.set_int('slider-value', (slider.value));
+    // const [hours, minutes] = timer.convertTime(_getTimerStartValue())
+    // switcher.label.text = hours + ' ' +_("h : ") + minutes + ' min';
+    
+    // if (settings.get_boolean('root-mode-value')) {
+    //     switcher.label.text = hours + ' ' +_("h : ") + minutes + ' min (root)'; 
+    // }
 }
+
+function _createSliderItem() {
+    let sliderValue =  _getInitialSliderValue()
+    let sliderItem = new PopupMenu.PopupMenuItem('');
+    let sliderIcon = new St.Icon({  icon_name: 'preferences-system-time-symbolic',  /*TODO: Change icons*/
+                                    style_class: 'popup-menu-icon' });
+    sliderItem.actor.add(sliderIcon);
+    slider = new Slider.Slider(sliderValue); /*TODO: check arguments */
+    slider.connect('notify::value', _onSliderChanged);
+    sliderItem.add_actor(slider);
+    return sliderItem;
+}
+
+function render() {
+    // submenu in status area menu with slider and toggle button
+    let sliderItem = _createSliderItem();    
+    submenu = new PopupMenu.PopupSubMenuMenuItem(_("Text Scaling Factor"), true);
+    submenu.icon.icon_name = 'system-shutdown-symbolic';
+    submenu.menu.addMenuItem(switcher);
+    submenu.menu.addMenuItem(sliderItem);
+    timer.setMenuLabel(submenu.label);
+
+    // add separator line and submenu in status area menu
+    separator = new PopupMenu.PopupSeparatorMenuItem();
+    let statusMenu = Main.panel.statusArea['aggregateMenu'];
+    statusMenu.menu.addMenuItem(separator);
+    statusMenu.menu.addMenuItem(submenu);
+}
+
+
+
+
+
+function init() {
+
+}
+
+function enable() {	
+    let gnomeSettings = _getGnomeSettings();
+	render()
+	settingsSignals.push(settings.connect('changed::slider-value', _onSettingsChanged));
+    log( "my double:" + settings.get_double('text-scaling-factor').toString() );
+  
+}
+
+function disable() {
+	submenu.destroy(); // destroys switcher and sliderItem as children too
+    separator.destroy();
+	settings.set_double('text-scaling-factor', 1.0);
+
+}
+
